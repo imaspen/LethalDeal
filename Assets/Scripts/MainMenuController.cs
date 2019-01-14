7a8a6b3c5ec82f2e7b814877fd091280bdb170f7
Aspen <zebington@gmail.com>
2019-01-14 16:40:49 +0000

    Created main menu and empty scenes

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+    private readonly string _gameScene = "Game Screen";
+    private readonly string _deckbuilderScene = "Deckbuilder";
+    private readonly string _optionsScene = "Options Menu";
+
+    public void onPlayClick()
+    {
+        SceneManager.LoadScene(_gameScene);
+    }
+
+    public void onDeckbuilderClick()
+    {
+        SceneManager.LoadScene(_deckbuilderScene);
+    }
+
+    public void onOptionsClick()
+    {
+        SceneManager.LoadScene(_optionsScene);
+    }
+
+    public void onQuitClicked()
+    {
+        Application.Quit();
+    }
+}
