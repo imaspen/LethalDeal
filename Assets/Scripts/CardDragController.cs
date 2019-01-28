bf1508548fca4fed399545d0d79ef8307396cdfc
Aspen <zebington@gmail.com>
2019-01-28 17:27:36 +0000

    added card spawning

--- Assets/Scripts/CardDragController.cs
+++ Assets/Scripts/CardDragController.cs
@@ -3,20 +3,23 @@
 public class CardDragController : MonoBehaviour
 {
     private Animator _animator;
+    private CardData _cardData;
+        
     private bool _isDragging;
     private Vector3 _mouseOffset;
 
     private Vector3 _startPosition;
+    
     public float DistanceToPlay;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _cardData = GetComponent<CardData>();
     }
 
     private void OnMouseDown()
     {
-        Debug.Log("hello");
         _animator.SetBool("MouseDragging", true);
         _isDragging = true;
         _startPosition = transform.parent.position;
@@ -30,7 +33,7 @@ public class CardDragController : MonoBehaviour
         _animator.SetBool("MouseDragging", false);
         _isDragging = false;
         if (transform.parent.position.y > _startPosition.y + DistanceToPlay)
-            Destroy(transform.parent.gameObject);
+            DoCard();
         else
             transform.parent.position = _startPosition;
     }
@@ -38,9 +41,15 @@ public class CardDragController : MonoBehaviour
     private void OnMouseDrag()
     {
         if (!_isDragging) return;
+        if (Camera.main == null) return;
         var mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y)) +
                        _mouseOffset;
-        Debug.Log(mousePos);
         transform.parent.position = mousePos;
+    }
+
+    private void DoCard()
+    {
+        Instantiate(_cardData.Spawns);
+        Destroy(transform.parent.gameObject);
     }
 }
