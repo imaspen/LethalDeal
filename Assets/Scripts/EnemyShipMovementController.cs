5c9f622b2f9d3d00513a30953db45750feeb1666
Aspen <zebington@gmail.com>
2019-02-02 21:58:49 +0000

    enemy ship added

---
+++ Assets/Scripts/EnemyShipMovementController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShipMovementController : MonoBehaviour
+{
+	public float ShipSpeed;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = new Vector3(
+			transform.position.x,
+			transform.position.y + ShipSpeed,
+			transform.position.z
+		);
+	}
+}
--- Assets/Scripts/Projectile/ProjectileEmitter.cs
+++ Assets/Scripts/Projectile/ProjectileEmitter.cs
@@ -9,7 +9,8 @@ namespace Projectile
         private Vector3 _emitterDirection;
 
         public int MaxShots;
-        public float AngularVelocity; //degrees per second
+        public float StartingRoation;
+        public float RotationPerShot; //degrees per second
         public float FixedUpdatesPerShot; //# of emits per second
         public float ProjectileInitialSpeed;
         public GameObject ProjectilePrefab;
@@ -17,25 +18,18 @@ namespace Projectile
         // Use this for initialization
         private void Start()
         {
-            _emitterDirection = gameObject.transform.up; //use the up axis in 2D as the forward direction
+            _emitterDirection = Quaternion.AngleAxis(StartingRoation, new Vector3(0.0f, 0.0f, 1.0f)) * transform.up;
         }
 
-        // Use fixed update to generate projectile at a fixed delta time
         private void FixedUpdate()
         {
             if (_elapsedFrames > FixedUpdatesPerShot && _shotsFired <= MaxShots)
             {
-                //Instantiate a projectile from a prefab
-                //Initial position is inherited from the parent, i.e. the emitter
-                //Rotation is Identity 
                 var projectileSpawn = Instantiate(ProjectilePrefab, transform.position, Quaternion.identity);
-
-                var rotation = Quaternion.AngleAxis(AngularVelocity * 1 / FixedUpdatesPerShot , new Vector3(0.0f, 0.0f, 1.0f));
+                var rotation = Quaternion.AngleAxis(RotationPerShot, new Vector3(0.0f, 0.0f, 1.0f));
 
                 projectileSpawn.GetComponent<Projectile>().Velocity = _emitterDirection * ProjectileInitialSpeed;
-
                 _emitterDirection = rotation * _emitterDirection;
-
                 if (MaxShots > 0) _shotsFired++;
                 _elapsedFrames = 0;
             }
