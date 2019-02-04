0d8a7b9222d7c4a5ee526d8462a4c656fd0dd241
G.Etherington U1874339 <U1874339@unimail.hud.ac.uk>
2019-02-04 16:49:25 +0000

    Added a gameover panel and working back to menu button

--- Assets/Scripts/Attacker/GunnerUIController.cs
+++ Assets/Scripts/Attacker/GunnerUIController.cs
@@ -6,17 +6,33 @@ namespace Attacker
 	public class GunnerUIController : UIController {
 		private Text _timerText;
 		private float _timer;
-	
-		protected override void Init()
+        public GameObject gameOverPanel;
+
+        protected override void Init()
 		{
 			_timerText = GameObject.Find("UI/Timer").GetComponent<Text>();
-			_timer = 60f;
-		}
+			_timer = 5f;
+            gameOverPanel = GameObject.Find("UI/GameOverPanel");
+      
+            gameOverPanel.SetActive(false);
+
+        }
 	
 		// Update is called once per frame
 		void Update () {
 			_timer -= Time.deltaTime;
 			_timerText.text = _timer.ToString("0.0");
+            
+            if (_timer <= 0.0)
+            {
+                GameOver();
+            }
 		}
+
+        void GameOver()
+        {
+            Time.timeScale = 0;
+            gameOverPanel.SetActive(true);
+        }
 	}
 }
