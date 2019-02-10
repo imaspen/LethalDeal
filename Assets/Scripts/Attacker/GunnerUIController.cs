113b71f39073d21ca245f28ab1497c3743df731b
Aspen <zebington@gmail.com>
2019-02-10 16:02:19 +0000

    Revert "George allen gunner ui stuff"

--- Assets/Scripts/Attacker/GunnerUIController.cs
+++ Assets/Scripts/Attacker/GunnerUIController.cs
@@ -6,33 +6,17 @@ namespace Attacker
 	public class GunnerUIController : UIController {
 		private Text _timerText;
 		private float _timer;
-        public GameObject gameOverPanel;
-
-        protected override void Init()
+	
+		protected override void Init()
 		{
 			_timerText = GameObject.Find("UI/Timer").GetComponent<Text>();
-			_timer = 5f;
-            gameOverPanel = GameObject.Find("UI/GameOverPanel");
-      
-            gameOverPanel.SetActive(false);
-
-        }
+			_timer = 60f;
+		}
 	
 		// Update is called once per frame
 		void Update () {
 			_timer -= Time.deltaTime;
 			_timerText.text = _timer.ToString("0.0");
-            
-            if (_timer <= 0.0)
-            {
-                GameOver();
-            }
 		}
-
-        void GameOver()
-        {
-            Time.timeScale = 0;
-            gameOverPanel.SetActive(true);
-        }
 	}
 }
