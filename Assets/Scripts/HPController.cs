9f1d37b27c0d1646cc8030b5e638033d5defaa00
A.Thompson U1862679 <U1862679@unimail.hud.ac.uk>
2019-02-11 15:45:19 +0000

    added basic damage handling

---
+++ Assets/Scripts/HPController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPController : MonoBehaviour {
+    public int MaxHP;
+
+    private int _currentHP;
+
+    public void Start()
+    {
+        _currentHP = MaxHP;
+    }
+
+    public void takeDamage(int amount)
+    {
+        _currentHP -= amount;
+        if (_currentHP > 0) return;
+        Die();
+    }
+
+    protected virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+}
